9b6e8506343851ccd316ce83f3f8c0595a1bfc60
Abey Onalaja <abey.onalaja@gmail.com>
2017-01-07 15:34:14 +0000

    Add templates and update create function on Image controller

--- web/controllers/image_controller.ex
+++ web/controllers/image_controller.ex
@@ -10,11 +10,23 @@ defmodule Imageer.ImageController do
 
   def new(conn,_) do
     changeset = Image.changeset(%Image{})
-    render(conn, "new.html", chnageset: changeset)
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"image" => image_params}) do
     IO.inspect image_params
+    changeset = Image.changeset(%Image{}, image_params)
+
+    case Repo.insert(changeset) do
+      {:ok, image} ->
+        conn
+        |> put_flash(:info, "Image was added")
+        |> redirect(to: image_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Something went wrong")
+        |> render("new.html", changeset: changeset)
+    end
   end
 
 
